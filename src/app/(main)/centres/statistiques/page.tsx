4efc8b3073b7651f29cebe805ded1ae7e4bf4bb8
ismail <ismailbouras1968@gmail.com>
2025-03-27 13:26:30 +0100

    api

--- src/app/(main)/centres/statistiques/page.tsx
+++ src/app/(main)/centres/statistiques/page.tsx
@@ -4,6 +4,7 @@ import { CentreStatistics } from "@/components/centre-statistics";
 export default function CentrePage() {
   return (
     <div className="container mx-auto pb-4 px-8">
+      <h1>Aucun statistique pour le moment</h1>
         {/**<CentreStatistics />**/}
     </div>
   );
